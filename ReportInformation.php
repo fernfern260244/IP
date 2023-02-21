4dd862ed2bacb79a4e1927959ddf82017351746b
Krapowkai <thyrt104@gmail.com>
2023-02-22 02:09:20 +0700

    add file php

---
+++ ReportInformation.php
@@ -0,0 +1,54 @@
+<?php include('server.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Report Information (รายละเอียดการรายงาน)</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav class="py-0 bg-amber-700 shadow-lg ">
+        <div class="container mx-auto px-2 flex justify-between ">
+            <div class="pr-20  ">
+                <a href="indext.html"><img class="image" src="images/logo.png" style="height: 80px;width: 80px;" ></a>
+            </div>
+            <div class=" flex flex-row space-x-4">  
+                
+                <div class="flex flex-row">
+                    <div><span><a href="shoppingCart.html"><img src="images/shop.png" style="width: 50px;height: 50px;"class="pl-6  pt-6"></a></span></div>
+                </div>
+                <div>
+                    <div><span><a href=""><img src="images/pro.png" style="width: 30px;height: 50px;" class="  pt-6"></a></span></div>
+                </div>
+            </div>
+        </div>
+       </nav>
+    <div class="container m-5">
+        <div class="flex justify-center">
+            <img src="images/pro01.jpg" class="p-3 rounded-full border " width="100" height="100">
+            <p  class="m-2">
+                ชื่อผู้ใช้ : สมศรี กระทืบสมปอง
+                <br> email address : somsrikrathubsompong@gmail.com
+            </p>
+        </div>
+
+    </div>
+    <div><p class="mx-5">รายละเอียดการรายงาน :</p></div>
+    <div class="container mx-auto p-5 border-2 grid place-items-center">
+        <div class=""> 
+            <p>
+                สร้างความสงสัยให้กับแฟนมวยชาวไทยเป็นอย่างมากสำหรับการที่ สหพันธ์กุนแขมร์นานาชาติ (KIF) หรือ มวยเขมร ที่จัดงานประชุมพิเศษก่อนการแข่งขันกีฬาซีเกมส์ 2023 ที่ประเทศกัมพูชา
+โดยทาง เจ้าภาพจัดการแข่งขันซีเกมส์ 2023 ออกมาประกาศยืนยันว่าจะไม่ขอใช้ชื่อ "มวยไทย" ในการแข่งขันครั้งนี้ แต่จะใช้ชื่อว่า "กุน แขมร์" ที่ถือเป็นกีฬาประจำชาติของพวกเขาแทน และไม่สนใจ สหพันธ์มวยไทยสมัครเล่นนานาชาติ (IFMA) ที่เคยออกมาขู่แบน
+            </p>
+        </div>
+    </div>
+    <div class="grid place-items-center m-5">
+        <button class="bg-amber-300 shadow-lg shadow-amber-200/50 w-auto rounded-full p-2 ">
+            ยืนยันการรายงาน
+        </button>
+    </div>
+</body>
+</html>
---
+++ alert.php
@@ -0,0 +1,65 @@
+<?php include('server.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav class="py-0 bg-amber-700 shadow-lg ">
+        <div class="container mx-auto px-2 flex justify-between ">
+            <div class="pr-20  ">
+                <a href="indext.html"><img class="image" src="images/logo.png" style="height: 80px;width: 80px;" ></a>
+            </div>
+            <div class=" flex flex-row space-x-4">  
+                
+                <div class="flex flex-row">
+                    <div><span><a href=""><img src="images/shop.png" style="width: 50px;height: 50px;"class="pl-6  pt-6"></a></span></div>
+                </div>
+                <div>
+                    <div><span><a href=""><img src="images/pro.png" style="width: 30px;height: 50px;" class="  pt-6"></a></span></div>
+                </div>
+            </div>
+        </div>
+       </nav>
+
+       <div class="flex flex-row ">
+        <div class="bg-orange-200 mt-5 mb-5 ml-32 rounded-md" style="width: 300px; height: 600px;">
+            <div class="mt-5 ml-14 rounded-md"><button class="rounded-md bg-amber-600 shadow-lg "style="height: 60px;width: 200px;" ><a href="createAccount.html">ที่ต้องจัดส่ง</a> </button></div>
+
+        </div>
+            <div class="bg-orange-200 ml-5 mt-5 mb-5 rounded-md" style="width: 900px; height: 600px;">
+                <div class="bg-orange-200 border-4 border-amber-500 flex flex-row" style="width:900px;height: 140px;">
+                    <div class="mt-5 ml-5" style="width: 100px;">
+                            <img style="width: 100px; height: 95px;" src="images/Celine-Mini-Ava-in-Tromphe-Canvas-and-Calfskin-Tan-Shoulder-Bag.jpg" alt="">
+                    </div>
+                    <div>
+                            <span><p class="text-2xl ml-14 mt-5">อยู่ระหว่างจัดส่ง</p></span>
+                            <span><p class="ml-14">หมายเลขพัสดุของคุณคือ </p></span>
+                            <span><p class="ml-14">ส่งผ่าน Standard Deriver-ส่งธรรมดาในประเทศ</p></span>
+                    </div>
+                    
+                </div>
+            </div>
+       </div>
+       
+
+
+
+
+
+
+       <footer class="footer footer-center p-10 bg-primary text-primary-content bg-orange-300 flex justify-center" >
+        <div>
+          <p class="font-bold flex justify-center text-center">
+               2MTrade <br/> Change second-hand goods
+          </p> 
+          <p>2MTrade © 2023 - All right reserved</p>
+        </div> 
+        
+      </footer>
+</body>
+</html>
